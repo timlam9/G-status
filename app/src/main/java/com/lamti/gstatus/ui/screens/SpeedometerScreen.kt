722ceff7ac47c43d50c814b0827d09291e98c9a4
timoleon <tilamprinos@gmail.com>
2023-09-10 18:35:53 +0300

    Add offset to speed value text

--- app/src/main/java/com/lamti/gstatus/ui/screens/SpeedometerScreen.kt
+++ app/src/main/java/com/lamti/gstatus/ui/screens/SpeedometerScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,6 +15,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 import com.lamti.gstatus.models.SpeedTest
 import com.lamti.gstatus.ui.components.Speedometer
@@ -21,6 +23,10 @@ import com.lamti.gstatus.ui.converters.toSpeedometerValue
 
 @Composable
 fun SpeedometerScreen(speedTest: SpeedTest) {
+    val configuration = LocalConfiguration.current
+    val screenWidth = configuration.screenWidthDp.dp
+    val speedValueOffset = -(screenWidth / 3)
+
     var speedometerValue by remember { mutableStateOf(0f) }
 
     LaunchedEffect(key1 = speedTest.downloadSpeed) {
@@ -33,8 +39,9 @@ fun SpeedometerScreen(speedTest: SpeedTest) {
     ) {
         Speedometer(value = speedometerValue)
         Text(
-            text = "${speedTest.downloadSpeed.value} Mbps | $speedometerValue UI units",
-            style = MaterialTheme.typography.labelLarge
+            text = "${speedTest.downloadSpeed.value} Mbps",
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.offset(y = speedValueOffset),
         )
         Spacer(modifier = Modifier.height(40.dp))
     }
