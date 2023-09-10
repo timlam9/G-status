62b8dce23d87f47327641ff1997947d2d7ec19f4
timoleon <tilamprinos@gmail.com>
2023-09-10 18:28:43 +0300

    Move speedometer value to corresponding screen

--- app/src/main/java/com/lamti/gstatus/ui/screens/SpeedometerScreen.kt
+++ app/src/main/java/com/lamti/gstatus/ui/screens/SpeedometerScreen.kt
@@ -1,20 +1,41 @@
 package com.lamti.gstatus.ui.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.lamti.gstatus.models.SpeedTest
+import com.lamti.gstatus.ui.components.Speedometer
+import com.lamti.gstatus.ui.converters.toSpeedometerValue
 
 @Composable
-fun SpeedometerScreen(speedometerValue: Float, speedTest: SpeedTest) {
-    com.lamti.gstatus.ui.components.Speedometer(value = speedometerValue)
-    Text(
-        text = "${speedTest.downloadSpeed.value} Mbps | $speedometerValue UI units",
-        style = MaterialTheme.typography.labelLarge
-    )
-    Spacer(modifier = Modifier.height(40.dp))
+fun SpeedometerScreen(speedTest: SpeedTest) {
+    var speedometerValue by remember { mutableStateOf(0f) }
+
+    LaunchedEffect(key1 = speedTest.downloadSpeed) {
+        speedometerValue = speedTest.downloadSpeed.value.toSpeedometerValue()
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Speedometer(value = speedometerValue)
+        Text(
+            text = "${speedTest.downloadSpeed.value} Mbps | $speedometerValue UI units",
+            style = MaterialTheme.typography.labelLarge
+        )
+        Spacer(modifier = Modifier.height(40.dp))
+    }
 }
